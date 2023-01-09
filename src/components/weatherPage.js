562938af131d3ad7e539cfca82e14bb9164fed36
Arman Ebrahimi <ebrahimi.arman72@gmail.com>
2023-01-09 14:27:53 -0800

    fixed warnings about bg audio

--- src/components/weatherPage.js
+++ src/components/weatherPage.js
@@ -31,21 +31,33 @@ export const WeatherPage = () => {
     },[weather])
 
     let climate = weather && weather.weather[0].main;
-    useEffect(() => {
+    useEffect( () => {
         let srcAudio;
-        switch (climate) {
-            case "Rain":
-                srcAudio = "";
-                break;
-            case "Thunderstorm":
-                srcAudio = "";
-                break;
-            case "Tornado":
-            case "Squall ":
-                srcAudio = "";
-                break;
-            default:
-                break;
+        if(climate){
+            if(climate === "Rain"){
+                srcAudio = "/Fluffing-a-Duck.mp3";
+            }
+            else if(climate === "Thunderstorm"){
+                srcAudio = "/mixkit-thunder-rumble-and-light-rain-2401.wav";
+            }
+            else if(climate === "Tornado" || climate === "Squall"){
+                srcAudio = "/mixkit-big-cinematic-impact-788.mp3";
+            }
+            else {
+                srcAudio = "/Fluffing-a-Duck.mp3";
+            }
+            const bgAudio = new Audio(srcAudio);
+            bgAudio.volume = 0.5;
+            async function fetchData(){
+                try{
+                    await bgAudio.play().then();
+                }
+                catch (e){
+                    console.log(e)
+                }
+            }
+            fetchData().then();
+            return bgAudio.pause();
         }
     }, [climate])
     return(
