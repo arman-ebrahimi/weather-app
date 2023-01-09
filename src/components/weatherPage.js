6d3278e095d9cf02074986885610e80f5f4f6f5e
Arman Ebrahimi <ebrahimi.arman72@gmail.com>
2023-01-08 16:12:58 -0800

    add some background-img for kinds of weather

--- src/components/weatherPage.js
+++ src/components/weatherPage.js
@@ -20,16 +20,19 @@ export const WeatherPage = () => {
     const sendRequest = () => {
         if(text){
             getSearchMethod(text);
-            fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${text}&APPID=${appId}&units=${units}`).then(
-                result => result.json()
-            ).then(result => setWeather(result))
+            fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${text}&APPID=${appId}&units=${units}`)
+                .then(result => result.json())
+                .then(result => setWeather(result))
         }
     }
+
     useEffect(() => {
         console.log(weather)
     },[weather])
+
+    let climate = weather && weather.weather[0].main;
     return(
-        <>
+        <div className={`weather-page ${weather && climate === "Clear" ? "clear" : weather && climate === "Clouds" ? "cloudy" : weather && (climate === "Rain" || climate === "Drizzle") ? "rainy" : weather && climate === "Thunderstorm" ? "thunder" : weather && climate === "Snow" ? "snow" : weather && (climate === "Fog" || climate === "Mist" || climate === "Haze") ? "foggy" : weather && climate === "Dust" ? "dust" : weather && (climate === "Tornado" || climate === "Squall") ? "tornado" : ""}`}>
             <div className="search-box">
                 <input type="text" onChange={handleChange} placeholder="City number or zip code" />
                 <button onClick={sendRequest}>Search</button>
@@ -40,9 +43,9 @@ export const WeatherPage = () => {
                     <div className="condition-row"><span>{weather.main.temp}&deg;</span><span>{weather.weather[0].description}</span><img src={"http://openweathermap.org/img/wn/" + weather.weather[0].icon + ".png"} alt="icon" /></div>
                     <hr />
                     <div>Humidity Levels at {weather.main.humidity}%</div>
-                    <div style={{marginTop: '5px'}}>Winds at {weather.wind.speed} m/s</div>
+                    <div style={{marginTop: '8px'}}>Winds at {weather.wind.speed} m/s</div>
                 </div>
             }
-        </>
+        </div>
     )
 }
