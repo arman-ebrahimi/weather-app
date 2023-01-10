3bc470e0c3bebe329460ab75b5d753aab8a9fc9b
Arman Ebrahimi <ebrahimi.arman72@gmail.com>
2023-01-09 17:11:56 -0800

    added a new voice and fix load voice and some change to when the city name doesn't find

--- src/components/weatherPage.js
+++ src/components/weatherPage.js
@@ -22,7 +22,13 @@ export const WeatherPage = () => {
             getSearchMethod(text);
             fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${text}&APPID=${appId}&units=${units}`)
                 .then(result => result.json())
-                .then(result => setWeather(result))
+                .then(result => {
+                    if(result.cod === "404"){
+                        alert(result.message.charAt(0).toUpperCase() + result.message.slice(1))
+                        return
+                    }
+                    setWeather(result)
+                })
         }
     }
 
@@ -34,32 +40,30 @@ export const WeatherPage = () => {
     useEffect( () => {
         let srcAudio;
         if(climate){
-            if(climate === "Rain"){
-                srcAudio = "/Fluffing-a-Duck.mp3";
-            }
-            else if(climate === "Thunderstorm"){
-                srcAudio = "/mixkit-thunder-rumble-and-light-rain-2401.wav";
-            }
-            else if(climate === "Tornado" || climate === "Squall"){
-                srcAudio = "/mixkit-big-cinematic-impact-788.mp3";
-            }
-            else {
-                srcAudio = "/Fluffing-a-Duck.mp3";
+            switch (climate) {
+                case "Rain":
+                    srcAudio = "/rainy.wav";
+                    break;
+                case "Thunderstorm":
+                    srcAudio = "/mixkit-thunder-rumble-and-light-rain-2401.wav";
+                    break;
+                case "Tornado":
+                case "Squall":
+                    srcAudio = "/mixkit-big-cinematic-impact-788.mp3";
+                    break;
+                case "Clear":
+                    srcAudio = "/birds.mp3";
+                    break;
+                default:
+                    return
             }
             const bgAudio = new Audio(srcAudio);
-            bgAudio.volume = 0.5;
-            async function fetchData(){
-                try{
-                    await bgAudio.play().then();
-                }
-                catch (e){
-                    console.log(e)
-                }
-            }
-            fetchData().then();
-            return bgAudio.pause();
+            bgAudio.volume = 0.2;
+            bgAudio.play().then();
+            return () => bgAudio.pause();
         }
-    }, [climate])
+    }, [climate, weather.name])
+
     return(
         <div className={`weather-page ${weather && climate === "Clear" ? "clear" : weather && climate === "Clouds" ? "cloudy" : weather && (climate === "Rain" || climate === "Drizzle") ? "rainy" : weather && climate === "Thunderstorm" ? "thunder" : weather && climate === "Snow" ? "snow" : weather && (climate === "Fog" || climate === "Mist" || climate === "Haze") ? "foggy" : weather && climate === "Dust" ? "dust" : weather && (climate === "Tornado" || climate === "Squall") ? "tornado" : ""}`}>
             <div className="search-box">
